9aae6b35a83a8f828625b053d60cbd094522429b
maprohu <maprohu@gmail.com>
2023-10-25 09:29:18 +0100

    Initialize more_menu

---
+++ src/menu.rs
@@ -0,0 +1,201 @@
+use crate::loading::{FontAssets, TextureAssets};
+use crate::GameState;
+use bevy::prelude::*;
+
+pub struct MenuPlugin;
+
+/// This plugin is responsible for the game menu (containing only one button...)
+/// The menu is only drawn during the State `GameState::Menu` and is removed when that state is exited
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Menu), setup_menu)
+            .add_systems(Update, click_play_button.run_if(in_state(GameState::Menu)))
+            .add_systems(OnExit(GameState::Menu), cleanup_menu);
+    }
+}
+
+#[derive(Component)]
+struct ButtonColors {
+    normal: Color,
+    hovered: Color,
+}
+
+impl Default for ButtonColors {
+    fn default() -> Self {
+        ButtonColors {
+            normal: Color::rgb(0.15, 0.15, 0.15),
+            hovered: Color::rgb(0.25, 0.25, 0.25),
+        }
+    }
+}
+
+#[derive(Component)]
+struct Menu;
+
+fn setup_menu(mut commands: Commands, font_assets: Res<FontAssets>, textures: Res<TextureAssets>) {
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    flex_direction: FlexDirection::Column,
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            Menu,
+        ))
+        .with_children(|children| {
+            children
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Px(140.0),
+                            height: Val::Px(50.0),
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            ..Default::default()
+                        },
+                        background_color: Color::rgb(0.15, 0.15, 0.15).into(),
+                        ..Default::default()
+                    },
+                    ButtonColors {
+                        normal: Color::rgb(0.15, 0.15, 0.15),
+                        hovered: Color::rgb(0.25, 0.25, 0.25),
+                    },
+                    ChangeState(GameState::Playing),
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Play",
+                        TextStyle {
+                            font: font_assets.fira_sans.clone(),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+            children
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Px(140.0),
+                            height: Val::Px(50.0),
+                            justify_content: JustifyContent::SpaceAround,
+                            align_items: AlignItems::Center,
+                            margin: UiRect {
+                                top: Val::Px(25.),
+                                ..default()
+                            },
+                            ..Default::default()
+                        },
+                        background_color: Color::NONE.into(),
+                        ..Default::default()
+                    },
+                    ButtonColors {
+                        normal: Color::NONE,
+                        hovered: Color::rgb(0.25, 0.25, 0.25),
+                    },
+                    OpenLink("https://bevyengine.org"),
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Made with Bevy",
+                        TextStyle {
+                            font: font_assets.fira_sans.clone(),
+                            font_size: 20.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                    parent.spawn(ImageBundle {
+                        image: textures.bevy_small.clone().into(),
+                        ..default()
+                    });
+                });
+            children
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Px(140.0),
+                            height: Val::Px(50.0),
+                            justify_content: JustifyContent::SpaceAround,
+                            align_items: AlignItems::Center,
+                            margin: UiRect {
+                                top: Val::Px(5.),
+                                ..default()
+                            },
+                            ..Default::default()
+                        },
+                        background_color: Color::NONE.into(),
+                        ..Default::default()
+                    },
+                    ButtonColors {
+                        normal: Color::NONE,
+                        hovered: Color::rgb(0.25, 0.25, 0.25),
+                    },
+                    OpenLink("https://github.com/NiklasEi/bevy_game_template"),
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Open source",
+                        TextStyle {
+                            font: font_assets.fira_sans.clone(),
+                            font_size: 20.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                    parent.spawn(ImageBundle {
+                        image: textures.bevy_small.clone().into(),
+                        ..default()
+                    });
+                });
+        });
+}
+
+#[derive(Component)]
+struct ChangeState(GameState);
+
+#[derive(Component)]
+struct OpenLink(&'static str);
+
+fn click_play_button(
+    mut next_state: ResMut<NextState<GameState>>,
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &ButtonColors,
+            Option<&ChangeState>,
+            Option<&OpenLink>,
+        ),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, button_colors, change_state, open_link) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                if let Some(state) = change_state {
+                    next_state.set(state.0.clone());
+                } else if let Some(link) = open_link {
+                    if let Err(error) = webbrowser::open(link.0) {
+                        warn!("Failed to open link {error:?}");
+                    }
+                }
+            }
+            Interaction::Hovered => {
+                *color = button_colors.hovered.into();
+            }
+            Interaction::None => {
+                *color = button_colors.normal.into();
+            }
+        }
+    }
+}
+
+fn cleanup_menu(mut commands: Commands, menu: Query<Entity, With<Menu>>) {
+    commands.entity(menu.single()).despawn_recursive();
+}
